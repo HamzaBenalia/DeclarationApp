5c7e0937176459dead8fe401dd6b8811882285f6
HamzaBenalia <hamzabenalia93@gmail.com>
2024-05-03 16:20:53 +0200

    FirstCommit

---
+++ src/main/java/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,33 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+
+    public static void main(String[] args){
+
+        Byte test1= 0;
+        Short test2= 1;
+
+        int test3= 2;
+
+        Long test4= 3L;
+
+        float test5= 4.0f;
+
+        double test6= 5.0;
+
+        boolean test7= true;
+
+        char test8= 'A';
+
+        String test9= "abc";
+
+
+        System.out.println(test9);
+
+
+        String randomString = "Voici le résultat d’un calcul : 1+5=6";
+
+        System.out.println(randomString);
+    }
+}
